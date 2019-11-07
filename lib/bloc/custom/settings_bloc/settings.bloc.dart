6c836383380debd935e070c223a1bd0e41975401
Curtis Schwoebel <curtis.schwoebel@iusinnovation.se>
2019-11-07 22:16:31 +0100

    add all project files

---
+++ lib/bloc/custom/settings_bloc/settings.bloc.dart
@@ -0,0 +1,94 @@
+import 'dart:async';
+import 'package:bloc_shared_prefs/bloc/bloc/bloc_event_state.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'settings.event.dart';
+part 'settings.state.dart';
+
+enum SettingsKeys { text_to_speech_rate, text_to_speech_pitch, app_language }
+SettingsBloc preferencesBloc = SettingsBloc();
+
+const Map<SettingsKeys, dynamic> defaults = {
+  SettingsKeys.app_language: 'sv-SE',
+  SettingsKeys.text_to_speech_rate: 1.0,
+  SettingsKeys.text_to_speech_pitch: 1.0
+};
+
+class SettingsBloc
+  extends BlocEventStateBase<SettingsEvent, SettingsState> {
+  SettingsBloc() {
+    _loadAppDefaults();
+    _loadSharedPreferences();
+  }
+
+  @override
+  SettingsState get initialState => SettingsState();
+
+  @override
+  Stream<SettingsState> eventHandler(
+    SettingsEvent event, SettingsState currentState) async* {
+    // TODO: implement eventHandler
+    if (event is SavePitchEvent) {
+      await _updateSharedPreference(
+        SettingsKeys.text_to_speech_pitch.toString(), event.pitch);
+    } else if (event is SaveRateEvent) {
+      await _updateSharedPreference(
+        SettingsKeys.text_to_speech_rate.toString(), event.rate);
+    } else if (event is SaveLanguageEvent) {
+      await _updateSharedPreference(
+        SettingsKeys.app_language.toString(), event.language);
+    }
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    yield SettingsState.updated(
+      updateLanguage: prefs.getString(
+        SettingsKeys.app_language.toString(),
+      ),
+      updatePitch: prefs.getDouble(
+        SettingsKeys.text_to_speech_pitch.toString(),
+      ),
+      updatedRate: prefs.getDouble(
+        SettingsKeys.text_to_speech_rate.toString(),
+      ),
+    );
+  }
+
+  SettingsBloc._internal();
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future<void> _loadAppDefaults() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    defaults.forEach(
+        (SettingsKeys key, dynamic value) async {
+        final _sharedPreference = sharedPrefs.get(key.toString());
+        if (_sharedPreference == null)
+          await _updateSharedPreference(
+            key.toString(), value);
+      },
+    );
+  }
+
+  Future<void> _loadSharedPreferences() async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    final _ttsPitchPref =
+    sharedPrefs.getDouble(SettingsKeys.text_to_speech_pitch.toString());
+    final _ttsRatePref =
+    sharedPrefs.getDouble(SettingsKeys.text_to_speech_rate.toString());
+    final _appLanguagePref =
+    sharedPrefs.getString(SettingsKeys.app_language.toString());
+    emitEvent(InitializedSettingsEvent(
+      _ttsPitchPref, _ttsRatePref, _appLanguagePref));
+  }
+
+  Future<void> _updateSharedPreference(String key, dynamic value) async {
+    final sharedPrefs = await SharedPreferences.getInstance();
+    if (value is double) {
+      sharedPrefs.setDouble(key, value);
+    } else {
+      sharedPrefs.setString(key, value);
+    }
+  }
+}
---
+++ lib/screens/settings.dart
@@ -0,0 +1,90 @@
+import 'package:bloc_shared_prefs/bloc/bloc/bloc_state_builder.dart';
+import 'package:bloc_shared_prefs/bloc/custom/settings_bloc/settings.bloc.dart';
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  String dropDownValue;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocEventStateBuilder<SettingsState>(
+        bloc: preferencesBloc,
+        builder: (BuildContext context, SettingsState state) {
+          return SafeArea(
+            bottom: false,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Slider(
+                  label: (state.ttsPitch / 100).toString(),
+                  divisions: 20,
+                  value: state.ttsPitch,
+                  onChanged: (double newValue) {
+                    preferencesBloc.emitEvent(
+                      SavePitchEvent(newValue),
+                    );
+                  },
+                  max: 100.0,
+                  min: 0.0,
+                ),
+                Slider(
+                  label: (state.ttsRate / 100).toString(),
+                  divisions: 20,
+                  value: state.ttsRate,
+                  onChanged: (double newValue) {
+                    preferencesBloc.emitEvent(
+                      SaveRateEvent(newValue),
+                    );
+                  },
+                  max: 100.0,
+                  min: 0.0,
+                ),
+                DropdownButton<String>(
+                  value: state.appLanguage,
+                  icon: Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  style: TextStyle(color: Colors.deepPurple),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  onChanged: (dynamic newValue) {
+                    preferencesBloc.emitEvent(
+                      SaveLanguageEvent(newValue),
+                    );
+                  },
+                  items: state.language_selection.keys.map((String key) {
+                    return DropdownMenuItem<String>(
+                      value: key,
+                      child: Text(
+                        state.language_selection[key],
+                      ),
+                    );
+                  }).toList(),
+                ),
+                Text(
+                  state.ttsPitch.toString(),
+                ),
+                Text(
+                  state.ttsRate.toString(),
+                ),
+                Text(state.appLanguage)
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
